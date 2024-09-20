b12af76508179e38cdbc53d81af41c779e7048bd
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-20 12:29:55 +0600

    transaction all complete

--- js/home2.js
+++ js/home2.js
@@ -4,6 +4,14 @@ document.getElementById('cash-in-btn').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = getInputValue('input-amount');
     const cashInPin = getInputValue('your-pin-number');
+
+    // number condition check 
+    if(isNaN(inputAmount)){
+        alert('Please Input valid Number');
+        return;
+    }
+
+    // pin number condition
     if(cashInPin === 1234){
         const accountBalance = getInputInner('account-balance');
         
@@ -12,14 +20,21 @@ document.getElementById('cash-in-btn').addEventListener('click', function(event)
         document.getElementById('account-balance').innerText = newBalance;
 
         // transaction section started
+
         const p = document.createElement('p');
         p.innerText = `Add Money: ${inputAmount} Tk..... New Balance: ${newBalance} Tk.`
 
         document.getElementById('transction-container').appendChild(p);
+
+        // input empty 
+        document.getElementById('input-amount').value = '';
+        document.getElementById('your-pin-number').value = '';
     }else{
         alert('oi mama na pls');
     }
 })
+
+
 
 // cash out function section 
 
@@ -29,9 +44,17 @@ document.getElementById('cash-out-btn').addEventListener('click', function(event
     const cashOutAmount = getInputValue('cash-out-amount');
     const cashOutPin = getInputValue('enter-pin-number');
 
+    if(isNaN(cashOutAmount)){
+        alert('Please Input Valid Number');
+        return;
+    }
     if(cashOutPin === 1234){
         const accountMainBalance = getInputInner('account-balance');
-        
+
+        if(cashOutAmount >accountMainBalance){
+            alert("Your Balance Low")
+            return;
+        }
         const accountNewBalance = accountMainBalance - cashOutAmount;
 
         document.getElementById('account-balance').innerText = accountNewBalance;
@@ -43,8 +66,11 @@ document.getElementById('cash-out-btn').addEventListener('click', function(event
             <h3>Cash Out Amount</h3>
             <p>${cashOutAmount}tk.. New Balance ${accountNewBalance}</p>
         `
-
         document.getElementById('transction-container').appendChild(div);
+
+        // input empty
+        document.getElementById('cash-out-amount').value = '';
+        document.getElementById('enter-pin-number').value = '';
 
     }else{
         alert('oi mama na pls');
